7855cf1dc74cdb0b3c616b0b7c3b69e2424075d2
Aviwe Ngqukumba <aviwe3226@gmail.com>
2021-04-09 12:43:41 +0200

    displaying top 10 commits on the commit viewer page (no avatar just yet)

--- src/components/commitViewer/CommitViewer.js
+++ src/components/commitViewer/CommitViewer.js
@@ -8,6 +8,7 @@ class CommitViewer extends Component {
   super(props);
   this.state = {
      commits: [],
+     repoName: ''
   };
 }
 
@@ -21,24 +22,50 @@ class CommitViewer extends Component {
 
   fetchRepoCommits = async (repoName) => {
    const repoCommits = await fetchRepoCommitsFromGithub(repoName);
-   this.setState({commits: repoCommits})
+   this.props.history.push({
+     state: {repoName}
+   })
+   this.setState({
+     commits: repoCommits,
+     repoName
+    })
   }
 
   render() {
-    const { repoName } = this.props.location.state;
-    const { commits } = this.state;
+    // const { repoName } = this.props.location.state;
+    const { commits, repoName } = this.state;
 
     let commitsView;
     if(commits.length === 0){
-      commitsView = (<p>Loading...</p>);
+      commitsView = (<p style={{textAlign: 'center'}}>Loading...</p>);
     }else {
-      commitsView = (<p>COOOMMMIITS...</p>)
+
+      commitsView = commits.map((commit, i) =>{
+        const date = new Date(commit.commit.author.date);
+        const dateString = ('0' + date.getHours()).slice(-2) + ':' +
+                           ('0' + date.getMinutes()).slice(-2) + ' '+('0' + date.getDate()).slice(-2) +
+                            '-' + ('0'+(date.getMonth()+1)).slice(-2) + '-' + date.getFullYear();
+        return (
+          <div key={i} className='row'>
+            <div className='column left' style={{background:'#aaa'}}>
+              <p>Some text..</p>
+            </div>
+            <div className='column middle' style={{background:'#bbb'}}>
+              <p>{commit.commit.message}</p>
+            </div>
+            <div className='column right' style={{background:'#ccc'}}>
+              <p>{dateString}</p>
+            </div>
+          </div>
+          )
+      }
+      )
     }
     return (
       <div>
-        <div className="navbar commit-view-header" style={{background: "#EFF2F6"}}>
+        <div className='navbar commit-view-header' style={{background: '#EFF2F6'}}>
           <h3>CommitViewer</h3>
-          <RepoSearchInput fetchRepoCommits={this.fetchRepoCommits}/>
+          <RepoSearchInput fetchRepoCommits={this.fetchRepoCommits} repoName={repoName}/>
         </div>
         <h3 className='repo-name'>{repoName || ''}</h3>
         <div>
